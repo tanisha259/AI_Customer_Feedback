e0e306a499819e288943fdba3fad511d6f598d46
Tanis <tanishapandit0@gmail.com>
2026-07-16 21:33:30 +0530

    build: add comment to postcss config

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
 export default {
   plugins: {
+    // Tailwind and Autoprefixer for CSS processing
     tailwindcss: {},
     autoprefixer: {},
   },
