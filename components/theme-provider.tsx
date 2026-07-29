8c201f85bb2963361c39a4de85c593ce5425b8b5
Tanis <tanishapandit0@gmail.com>
2026-07-29 21:30:29 +0530

    refactor(components): remove trailing comment in theme-provider

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -7,4 +7,3 @@ import { type ThemeProviderProps } from "next-themes";
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
-// Note: The ThemeProvider relies on next-themes for client-side theme switching.
